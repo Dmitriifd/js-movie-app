fe7ac601174e7e57b712ba285a95fdf4b0ace0cc
Dmitriifd <idimka9@gmail.com>
2023-04-01 21:36:07 +0500

    search modal js

--- assets/js/movie-list.js
+++ assets/js/movie-list.js
@@ -3,6 +3,7 @@
 import { api_key, fetchDataFromServer } from './api.js';
 import { sidebar } from './sidebar.js';
 import { createMovieCard } from './movie-card.js';
+import { search } from './search.js';
 
 // collect genre name & url parameter from local storage
 const genreName = window.localStorage.getItem('genreName');
@@ -59,3 +60,5 @@ fetchDataFromServer(fetchURL, function ({ results: movieList, total_pages }) {
     });
   });
 });
+
+search();
