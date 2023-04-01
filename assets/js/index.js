15ee7ce39b154a1ac6a80df61c378c40e12ca92d
Dmitriifd <idimka9@gmail.com>
2023-04-01 15:56:30 +0500

    top rated, upcoming, trending movies

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,28 @@
 
 import { sidebar } from './sidebar.js';
 import { api_key, imageBaseURL, fetchDataFromServer } from './api.js';
+import { createMovieCard } from './movie-card.js';
 
 const pageContent = document.querySelector('[page-content]');
 
 sidebar();
+
+// Home Page sections (Top rated, Upcoming, Trending movies )
+
+const homePageSections = [
+  {
+    title: 'Upcoming Movies',
+    path: '/movie/upcoming',
+  },
+  {
+    title: 'Weekly Trending Movies',
+    path: '/trending/movie/week',
+  },
+  {
+    title: 'Top Rated Movies',
+    path: '/movie/top_rated',
+  },
+];
 
 const genreList = {
   asString(genreIdList) {
@@ -95,6 +113,15 @@ const heroBanner = function ({ results: movieList }) {
   pageContent.appendChild(banner);
 
   addHeroSlide();
+
+  // Fetch data for home page sections (top rated, upcoming, trending)
+  for (const { title, path } of homePageSections) {
+    fetchDataFromServer(
+      `https://api.themoviedb.org/3${path}?api_key=${api_key}&page=1`,
+      createMovieList,
+      title
+    );
+  }
 };
 
 // Hero slider
@@ -124,4 +151,25 @@ const addHeroSlide = function () {
   };
 
   addEventOnElements(sliderControls, 'click', sliderStart);
+};
+
+const createMovieList = function ({ results: movieList }, title) {
+  const movieListElem = document.createElement('section');
+  movieListElem.classList.add('movie-list');
+  movieListElem.ariaLabel = `${title}`;
+  movieListElem.innerHTML = `
+    <div class="title-wrapper">
+      <h3 class="title-large">${title}</h3>
+    </div>
+    <div class="slider-list">
+      <div class="slider-inner"></div>
+    </div>
+    `;
+
+  for (const movie of movieList) {
+    const movieCard = createMovieCard(movie);
+    movieListElem.querySelector('.slider-inner').appendChild(movieCard);
+  }
+  
+  pageContent.appendChild(movieListElem);
 };
